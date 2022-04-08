f1ad8c9223080ea3bce44a6bd224e619d36a423a
VassiliKan <sachacauli@gmail.com>
2022-04-08 10:41:13 +0200

    pb V3 ack à régler

--- src/mictcp.c
+++ src/mictcp.c
@@ -1,9 +1,17 @@
+/*
+
+
+Pb ack / seq num : séquence Abandon abandon abandon retry (n fois éventuellement)
+Dans le cas d'une perte
+
+*/
+
 #include <mictcp.h>
 #include <api/mictcp_core.h>
 
 #define LOSS_RATE 1
-#define LOSS_WINDOW_SIZE 100
-#define LOSS_ACCEPTANCE 2
+#define LOSS_WINDOW_SIZE 10
+#define LOSS_ACCEPTANCE 30
 #define MAX_TRY_CONNECT 15
 
 protocol_state client_state;
@@ -23,8 +31,8 @@ int nb_send = 0;
 int effective_loss_rate = 0;
 
 /////////////////////
-int lossWindow[LOSS_WINDOW_SIZE];
-int lossWindowIndex = 0;
+int loss_window[LOSS_WINDOW_SIZE]= {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int loss_window_index = 0;
 
 
 /*
@@ -50,11 +58,7 @@ int mic_tcp_socket(start_mode sm) {
         num_socket++;
     }
 
-    //initialisation du tableau de pertes
-    for (int i =0; i<LOSS_WINDOW_SIZE; i++){
-        lossWindow[i]=0;
-    }
-   return result;
+    return result;
 }
 
 /*
@@ -87,8 +91,7 @@ int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr) {
 int mic_tcp_connect(int socket, mic_tcp_sock_addr addr) {
     
     printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
-    
-    /*
+   /* 
     mic_tcp_pdu pdu_send;
     mic_tcp_pdu pdu_recv;
     int res_send;
@@ -111,8 +114,9 @@ int mic_tcp_connect(int socket, mic_tcp_sock_addr addr) {
 
     } while (res_recv == -1 && nb_try < MAX_TRY_CONNECT);
       
+    client_state = SYN_SENT;
 
-    // envoie ACK
+    // envoie ACK dans process pdu ?
 */
     return 0;
 }
@@ -129,7 +133,7 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size) {
     int res_recv;
     int retry = 0;
 
-    printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
+ //   printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
     
     if (my_socket.fd != mic_sock){
         return -1;
@@ -147,34 +151,34 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size) {
     
     do {
 
-        if (lossWindowIndex == LOSS_WINDOW_SIZE) { //cyclage du tableau de pertes
-            lossWindowIndex = 0;
+        if (loss_window_index == LOSS_WINDOW_SIZE) { //cyclage du tableau de pertes
+            loss_window_index = 0;
         }
 
         res_send = IP_send(pdu_send, my_socket.addr_dist); 
         res_recv = IP_recv(&pdu_recv, NULL, 100);
         
+        printf("ENVOI %d ", nb_send);
         if(res_recv == -1 || pdu_recv.header.ack_num != seq_num) { //echec de reception de l'acquittement ou mauvais numero d'acquittement reçu
-            lossWindow[lossWindowIndex] = 1;
-            printf("echec envoi\n");
-
+            printf("ECHEC : ");
+            loss_window[loss_window_index] = 1;
             effective_loss_rate = calcul_loss_rate();
             
-            if(effective_loss_rate > LOSS_ACCEPTANCE){ //abandon de l'envoi apres trop d'echec
-                retry = 0;
-                printf("on abandonne\n");
-            } else {
+            // Cas trop de pertes, on renvoie
+            if(effective_loss_rate > LOSS_ACCEPTANCE){ 
+                printf("RETRY\n");                
                 retry = 1;
-                printf("on reessaie\n");
+            } else {
+                printf("ABANDON\n");
+                retry = 0;
             }    
-           // printf("send : %d ; rate : %d\n", nb_send, effective_loss_rate);
-        } else {  //reussite de l'envoi
-            lossWindow[lossWindowIndex]=0;
+        } else {  // succès envoie
+            printf("SUCCES\n");
+            loss_window[loss_window_index] = 0;
             retry = 0;
-            printf("reussite envoi\n");
         }
 
-        lossWindowIndex++;
+        loss_window_index++;
 
     } while (retry);
     
@@ -184,11 +188,14 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size) {
 }
 
 
-
+/*
+ * Calcule le taux de pertes sur la fenêtre glissante 
+ *  Renvoie un entier correspondant à ce taux (en %)
+ */
 int calcul_loss_rate(){
-    int loss;
-    for (int i = 0; i<LOSS_WINDOW_SIZE; i++){ //calcul du loss rate
-        loss += lossWindow[i];
+    int loss = 0;
+    for (int i = 0; i < LOSS_WINDOW_SIZE; i++){ //calcul du loss rate
+        loss += loss_window[i];
     }
     return loss * 100 / LOSS_WINDOW_SIZE;
 }
